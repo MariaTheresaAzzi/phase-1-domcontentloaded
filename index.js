def788c9529e88fb78321c26d4829c7342670a9d
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-08-02 21:12:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", (event) => {
+    onDOMContentLoaded();
+    console.log("The DOM has loaded");
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+
+const event = document.createEvent('Event');
+
+onDOMContentLoaded = (event) => {
+    const text = document.getElementById('text').textContent = "This is really cool!";
+};
+
+
